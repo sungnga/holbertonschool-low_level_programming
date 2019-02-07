74888c16cd69c610c4c6a6af1b090aa125b30348
sungnga <708@holbertonschool.com>
2019-02-07 08:03:44 +0000

    adding 5-printf.c and 5-printf.o files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-
-int main(void)
+#include<stdio.h>
+int main()
 {
-  printf("with proper grammar, but the outcome is a piece of art, \n");
-  return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return 0;
 }

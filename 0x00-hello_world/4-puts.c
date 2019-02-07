a8c8b43fd0aae02ce0ebf41f30dfbc3de81c8740
sungnga <708@holbertonschool.com>
2019-02-07 21:32:28 +0000

    Adding 4-puts.c and 4-puts.o files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints ""Programming is like..."
+ * Return: int value of zero
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

ec0a25e47800bfafe057a3185eb41f3ae69864fb
sungnga <708@holbertonschool.com>
2019-02-08 07:49:49 +0000

    Adding a 7-print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * ch is a char type
+ * Return: a zero value
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+		putchar('\n');
+	return (0);
+}

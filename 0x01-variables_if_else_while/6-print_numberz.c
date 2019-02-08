382b5f576cd8e5c7967b832d9447b1f5522ec62d
sungnga <708@holbertonschool.com>
2019-02-08 07:46:34 +0000

    Adding a ./6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * ch is a char type
+ * Return: a zero value
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+		putchar('\n');
+	return (0);
+}

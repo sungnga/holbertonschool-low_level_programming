c8400f4ca19b2b98ae90a1e1cc9df78995252e00
sungnga <708@holbertonschool.com>
2019-02-08 23:41:19 +0000

    Adding a 100-print_comb3.c file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,22 @@
  */
 int main(void)
 {
-        int d1;
-        int d2;
+	int d1;
+	int d2;
 
-        for (d1 = 0; d1 <= 8; d1++)
-        {
-                for(d2 = d1 + 1; d2 <= 9; d2++)
-                {
-                        putchar(d1 + '0');
-                        putchar(d2 + '0');
-                        if (d1 != 8){
+	for (d1 = 0; d1 <= 8; d1++)
+	{
+		for (d2 = d1 + 1; d2 <= 9; d2++)
+		{
+			putchar(d1 + '0');
+			putchar(d2 + '0');
+			if (d1 != 8)
+			{
 				putchar(',');
 				putchar(' ');
-                        }
-                }
-        }
+			}
+		}
+	}
 	putchar('\n');
-        return (0);
+	return (0);
 }

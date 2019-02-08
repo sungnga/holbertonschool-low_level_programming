36f98380c62788f6c40c52ba4d164f4461180f82
sungnga <708@holbertonschool.com>
2019-02-08 02:40:34 +0000

    Adding a 1-last_digit.c file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints random number and prints the last digit
+ * n is an integer type
+ * a is an integer type
+ * Return: a zero value
+ */
+int main(void)
+{
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	print("Last digit of %d is", n);
+	a = n % 10;
+	if (a > 5)
+		printf("%d and is greater than 5\n", a);
+	if (a == 0)
+		printf("%d and is 0\n", a);
+	if (a < 6 && a != 0)
+		printf("%d and is less than 6 and not 0\n", a);
+return (0);
+}

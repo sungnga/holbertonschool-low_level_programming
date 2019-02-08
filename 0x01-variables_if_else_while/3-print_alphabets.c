c549911cb18781755f903bb5bdad834832aa3171
sungnga <708@holbertonschool.com>
2019-02-08 06:27:59 +0000

    Adding a 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * ch is a char type
+ * Return: a zero value
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}

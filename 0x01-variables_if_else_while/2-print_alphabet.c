a57f511b4e1c91ebc764df6ad7cab0f917dd659d
sungnga <708@holbertonschool.com>
2019-02-08 03:56:42 +0000

    Adding a 2-print_alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * ch is a char type
+ * Return: a zero value
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+       	}
+	return (0);
+}

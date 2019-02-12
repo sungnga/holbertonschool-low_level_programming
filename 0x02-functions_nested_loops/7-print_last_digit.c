7b892c295c7675c2ddbea4b0dbac050b54a9ca93
sungnga <708@holbertonschool.com>
2019-02-12 17:15:08 +0000

    Adding a 7-print_last_digit.c file

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * r - integer type
+ * Return: r integer value
+ */
+
+int print_last_digit(int r)
+{
+	r = (r % 10);
+	if (r < 0)
+		r = (r * -1);
+	_putchar(r + '0');
+	return (r);
+}

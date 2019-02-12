c61b35d744e77da3249eab9896bb34a3255f89e2
sungnga <708@holbertonschool.com>
2019-02-12 19:25:03 +0000

    Adding a 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @r: is integer type
+ * Return: r in integer value
+ */
+int _abs(int r)
+{
+	if (r > 0)
+		return (r);
+	else
+	{
+		(r = r * -1);
+	}
+	return (r);
+}

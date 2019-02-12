787da57095c59ab6ec21c725e81f01315d171502
sungnga <708@holbertonschool.com>
2019-02-12 23:05:35 +0000

    Adding a 8-24_hours.c file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,25 +8,17 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 59; j++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+       	       		_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }

b3db607d9f14476a3e58d986d506ba1bf4adcf92
sungnga <708@holbertonschool.com>
2019-02-12 08:37:57 +0000

    Adding a 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints 10x the alphabet in lowercase followed by
+ * a new line
+ * ch is a char type
+ * i is a integer type
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+}
+}

8763c7d8384db36ddc9a4e3ba204589d2a5c132f
sungnga <708@holbertonschool.com>
2019-02-14 20:37:53 +0000

    Adding a 5-more_numbers.c file

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_more_numbers - prints 10x the numbers from 0 to 14, followed by
+ * a new line
+ *
+ * t - integer type
+ * i - integer type
+ * @void: no return
+ */
+void more_numbers(void)
+{
+	int i;
+	int t;
+
+	for (t = 0; t <=9; t++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i % 10 + '0');
+			
+				_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}

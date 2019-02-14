4f18c3bff737f3e425f50bfe151b1c717721fd49
sungnga <708@holbertonschool.com>
2019-02-14 01:18:44 +0000

    Adding a 104-fibonacci.c file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * a - long type
+ * b - long type
+ * c - long type
+ * i - long type
+ * Return: 0 integer value
+ */
+int main(void)
+{
+	long a;
+	long b;
+	long c;
+	long i;
+
+	a = 1;
+	b = 2;
+
+	printf("%lu, %lu", a, b);
+	for (i = 0; i < 95; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}

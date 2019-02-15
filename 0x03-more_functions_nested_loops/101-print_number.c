0c80fe22b5a67cc6beac9b4815dcb268b0321036
sungnga <708@holbertonschool.com>
2019-02-15 22:32:00 +0000

    Adding a 101-print_number.c file

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -2,12 +2,20 @@
 
 /**
  * print_number - prints an integer
- * @n: integer type
- * i - integer type
- * Return: 0 integer value
+ * @n: integer unsigned type
  */
 void print_number(int n)
 {
+	unsigned int n1;
 
-
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+		n1 = n;
+	}
+	n1 = n;
+	if (n1 / 10)
+		print_number(n1 / 10);
+	_putchar(n1 % 10 + '0');
 }

d1aa162c066fd778b4595b0c7fe634b048a3110b
sungnga <708@holbertonschool.com>
2019-02-19 18:33:48 +0000

    Adding a 2-strlen.c file

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: character type
+ * i - integer type
+ * Return: integer type
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}

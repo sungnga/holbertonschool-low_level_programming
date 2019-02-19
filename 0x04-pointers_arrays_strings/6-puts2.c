4424561eb6be49e7e0029e0a69fe7bd8eead5026
sungnga <708@holbertonschool.com>
2019-02-19 22:08:04 +0000

    Adding a 6-puts2.c file

---
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+int _strlen(char *s);
+/**
+ * _strlen - returns the length of a string
+ * @s: character type
+ * i - integer type
+ * Return: integer type
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * puts2 - prints one char out of 2 of a string, followed by a new line
+ * @str: character type
+ * i - integer type
+ * len - integer type
+ */
+void puts2(char *str)
+{
+	int i;
+	int len = _strlen(str);
+
+	for (i = 0; i <= len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}

5d54a10ae5dc9f06f951582a58d5cd0a5097b05f
sungnga <708@holbertonschool.com>
2019-02-25 23:51:31 +0000

    Adding a _memcpy function file

--- 0x06-pointers_arrays_strings/1-memcpy.c
+++ 0x06-pointers_arrays_strings/1-memcpy.c
@@ -2,25 +2,22 @@
 
 /**
  * _memcpy - copies memory area
- * The _memset() - funct copies n bytes from memory area src to memory area dest
+ * _memset() - funct copies n bytes from memory area src to memory area dest
  * @dest: char type
  * @src: char type
  * @n: unsigned int type
- * i - integer type
  * Return: a pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *cdest = dest;
 	char *csrc = src;
-/*	unsigned int i; */
 
 	while (n)
 	{
 		*(cdest++) = *(csrc++);
 		n--;
 	}
-/*	for (i = 0; i < n; i++)
-	dest[i] = '\n'; */
+
 	return (dest);
 }

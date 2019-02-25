dea62f477609bda096a919cd639a7a2766949e0e
sungnga <708@holbertonschool.com>
2019-02-25 22:53:48 +0000

    Adding a _memset function file

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * The _memset() - funct fills the first n bytes of the memory area pointed to
+ * by s with the constant byte b
+ * @s: char type
+ * @b: char type
+ * @n: unsigned int type
+ * Return: 0 integer value, a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = s[i] + b;
+	return (0);
+}

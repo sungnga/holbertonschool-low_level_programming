4a7c73a8e5ce20ff36988df15d0ec8272404b239
sungnga <708@holbertonschool.com>
2019-02-26 06:51:20 +0000

    Adding a _strpbrk function file

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: char type
+ * @accept: char type
+ * i - integer type
+ * j - integer type
+ * Return: a pointer to the byte in s or NULL if no such byte
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+	return (0);
+}

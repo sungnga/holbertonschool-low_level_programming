5291c5a9904a24bc51131d145b257a812e9133e2
sungnga <708@holbertonschool.com>
2019-02-26 02:50:05 +0000

    Adding a _strchr function file

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: char type
+ * @c: char type
+ * i - integer type
+ * Return: a pointer of char c or 0 integer value if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+			return (0);
+}

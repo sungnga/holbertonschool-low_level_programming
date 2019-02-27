ced516bae7b676ae1cef1a04d54e58bdaa19bb0d
sungnga <708@holbertonschool.com>
2019-02-27 20:51:34 +0000

    Adding a print string using recursive

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursive - prints a string, followed by a new line
+ * @s: char type
+ * Return: no return
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return ;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+}

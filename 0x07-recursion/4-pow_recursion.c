9218504dbb65ee3530d6bac825fcabb0f8e41686
sungnga <708@holbertonschool.com>
2019-02-27 22:18:27 +0000

    Returns the value of x raised to the power of y

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: integer type
+ * @y: integer type
+ * Return: integer type
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+		_putchar('\n');
+	}
+	return (_pow_recursion(x, y - 1) * x);
+}

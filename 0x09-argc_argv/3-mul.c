cbb1f6faa62b6345146f7fcd38897e377fa7891e
sungnga <708@holbertonschool.com>
2019-03-04 08:29:29 +0000

    A program that multiplies 2 numbers

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 
-	if ( argc != 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -25,5 +25,5 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[2]);
 	result = num1 * num2;
 	printf("%d\n", result);
-        return (0);
+	return (0);
 }

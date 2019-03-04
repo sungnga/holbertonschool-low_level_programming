428400583b7d2e0e1a7dd74c21e5090b60d4bd6c
sungnga <708@holbertonschool.com>
2019-03-04 07:36:15 +0000

    a program that prints the num of arg passed into it

--- 0x09-argc_argv/1-args.c
+++ 0x09-argc_argv/1-args.c
@@ -6,12 +6,21 @@
  * main - prints its name, followed by a new line
  * @argc: integer type
  * @argv: char type
+ * i - integer type
+ * sum - integer type
  * Return: 0 value integer
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
+	int i;
+	int sum;
 
-	printf("%d\n", argc);
+	if (argc >= 0)
+	{
+		for (i = 0; i < argc; i++)
+	       		sum += i;
+	}
+	printf("%d\n", sum);
 	return (0);
 }

5e890dc3b2054b5390ce0b28a13fa8a0a8344484
sungnga <708@holbertonschool.com>
2019-03-04 07:09:30 +0000

    Prints its name using argc and argv

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -6,15 +6,13 @@
  * main - prints its name, followed by a new line
  * @argc: integer type
  * @argv: char type
- * i - integer type
  * Return: 0 value integer
  */
 int main(int argc, char *argv[])
 {
-
-	if (argc > 0)
+	if (argc)
 	{
-		printf("%s\n", argv[argc]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }

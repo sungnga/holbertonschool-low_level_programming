8eea131cece1df871d9bf7ded514a5a91666e525
sungnga <708@holbertonschool.com>
2019-03-04 07:20:52 +0000

    a program that prints the num of arg passed into it

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: integer type
+ * @argv: char type
+ * Return: 0 value integer
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}

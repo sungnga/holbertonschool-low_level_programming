dedd7a03ade55336fe5ce643ae83a41675497efb
sungnga <708@holbertonschool.com>
2019-03-05 20:56:03 +0000

    A function that creates an array of chars

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars, and initializes it with a
+ * specific char
+ * @size: unsigned integer type
+ * @c: char type
+ * Return: a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *new_arr = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		new_arr[i] = c;
+	return (new_arr);
+}

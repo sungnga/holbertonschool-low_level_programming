9f52edd26c303b0a53f5bab160596c05f61d9ebd
sungnga <708@holbertonschool.com>
2019-03-14 07:42:38 +0000

    Initial commit

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ * op_add - prints the result of the operation, followed by new line
+ * @a: first number integer
+ * @b: second number integer
+ * Return: integer type
+ */
+int op_add(int a, int b)
+{
+
+}

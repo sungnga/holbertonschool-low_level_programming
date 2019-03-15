a1caf78f8c40de1e19a0d9b2b7d48459c6a64b99
sungnga <708@holbertonschool.com>
2019-03-15 04:46:31 +0000

    recommit

--- 0x0E-function_pointers/3-op_functions.c
+++ 0x0E-function_pointers/3-op_functions.c
@@ -1,26 +1,66 @@
 #include "3-calc.h"
 
+/**
+ * op_add - returns the sum of a and b
+ * @a: first number in integer
+ * @b: second number in integer
+ * Return: the result
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_add - returns the difference of a and b
+ * @a: first number in integer
+ * @b: second number in integer
+ * Return: the result
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_add - returns the product of a and b
+ * @a: first number in integer
+ * @b: second number in integer
+ * Return: the result
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_add - returns the remainder of the division of a and b
+ * @a: first number in integer
+ * @b: second number in integer
+ * Return: the result
+*/
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+                exit (100);
+	}
 	return (a / b);
 }
 
+/**
+ * op_add - returns the sum of a and b
+ * @a: first number in integer
+ * @b: 2nd number in integer
+ * Return: the result
+*/
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+                exit (100);
+	}
 	return (a % b);
 }

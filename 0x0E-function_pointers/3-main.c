7acfb5fde3e438dfd430111cd533db49d3dc04c6
sungnga <708@holbertonschool.com>
2019-03-15 05:11:17 +0000

    change comment and remove a space for exit declaration

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * op_add - prints the result of the operation, followed by new line
+ * main - prints the result of the operation, followed by new line
  * @argc: counter for number arguments
  * @argv: pointer to the value of the input numbers
  * Return: integer type
@@ -13,13 +13,13 @@ int main(int argc, char **argv)
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	ptr = get_op_func(argv[2]);
 	if (!ptr)
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 
 	printf("%d\n", ptr(atoi(argv[1]), atoi(argv[3])));

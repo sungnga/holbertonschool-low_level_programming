0f524843870d4c651351d0b0b265bcb92fd8fcb4
sungnga <708@holbertonschool.com>
2019-03-15 04:44:47 +0000

    recommit

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -1,13 +1,27 @@
-#include "function_pointers.h"
 #include "3-calc.h"
 
 /**
  * op_add - prints the result of the operation, followed by new line
- * @a: first number integer
- * @b: second number integer
+ * @argc: counter for number arguments
+ * @argv: pointer to the value of the input numbers
  * Return: integer type
  */
-int op_add(int a, int b)
+int main(int argc, char **argv)
 {
+	int (*ptr)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	ptr = get_op_func(argv[2]);
+	if (!ptr)
+	{
+		printf("Error\n");
+		exit (99);
+	}
+
+	printf("%d\n", ptr(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }

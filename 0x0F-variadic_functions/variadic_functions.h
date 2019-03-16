338371031d5aad66348867e00a9e2cd9648e4e85
sungnga <708@holbertonschool.com>
2019-03-16 19:05:06 +0000

    Adding typedef function

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -3,10 +3,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct types - Struct types
+ *
+ * @s: character of types
+ * @function: The function associated
+ */
+typedef struct types
+{
+	char *s;
+	void (*function)(va_list list);
+} list_types;
 
 #endif /* VARIADIC_FUNCTIONS_H */

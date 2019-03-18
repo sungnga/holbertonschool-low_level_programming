0c0d6dd489fa968ed527503980152fb1ac016949
sungnga <708@holbertonschool.com>
2019-03-18 01:38:43 +0000

    added helper functions prototypes

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -10,10 +10,14 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+void ptr_char(va_list list);
+void ptr_int(va_list list);
+void ptr_float(va_list list);
+void ptr_string(va_list list);
+
 /**
  * struct types - Struct types
- *
- * @s: character of types
+ * @s: character pointer of types
  * @function: The function associated
  */
 typedef struct types

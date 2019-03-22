060b6b9ff6589e7cfda89675af419e9cb2932b02
sungnga <708@holbertonschool.com>
2019-03-22 07:26:09 +0000

    initial commit

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,25 @@
+    section .data
+	fmt     db "%u  %s",10,0
+	msg1    db "Hello",0
+	msg2    db "Holberton",0
+
+	    section .text
+	    extern printf
+	    global _start
+
+_start:
+	    mov  edx, msg1
+	    mov  esi, 1
+	    mov  edi, fmt
+	    mov  eax, 0     	; no f.p. args
+	    call printf
+
+	    mov  edx, msg2
+	    mov  esi, 2
+	    mov  edi, fmt
+	    mov  eax, 0     	; no f.p. args
+	    call printf
+
+	    mov  ebx, 0     	; return value
+	    mov  eax, 1
+	    int  0x80

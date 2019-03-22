75d11ed5627f8c54a64b2f9bc6dc0b9a08e9c3f4
sungnga <708@holbertonschool.com>
2019-03-22 07:46:18 +0000

    redo code

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,12 @@
-.string "Hello, Holberton\n"
-fmt:	  .string "char: %c \nint: %d\n"
+hello:	 .string "Hello, Holberton\n"
+
 	.balign 4
 	.global main
 main:	 stp  x29, x30, [sp, -16]!
 	mov  x29, sp
-	mov  w19, 65
-	mov  w20, 42
 
-	adrp x0, fmt
-	add  x0, x0, :lo12:fmt
-	mov  w1, w19
-	mov  w2, w20
+	adrp x0, hello
+	add  x0, x0, :lo12:hello
 	bl   printf
 
 	mov  w0, 0

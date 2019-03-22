476f4eba330c025440be72b014a55f476869945e
sungnga <708@holbertonschool.com>
2019-03-22 06:03:50 +0000

    initial commit

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: a pointer to the first node of list_t list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		free(head->str);
+       		free(head);
+	}
+}

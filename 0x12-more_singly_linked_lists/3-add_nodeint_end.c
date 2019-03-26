40b767f3504c024b369140d2e501dab418d24a24
sungnga <708@holbertonschool.com>
2019-03-26 18:55:35 +0000

    removed empty spaces

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -28,5 +28,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	while (last->next != NULL)
 		last = last->next;
 	last->next = new_node;
-        return (new_node);
+	return (new_node);
 }

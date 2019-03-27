d26eb0815bafd9b47a31fa032645edab820c288d
sungnga <708@holbertonschool.com>
2019-03-27 06:52:21 +0000

    added an if statment to ptr

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	ptr = *head;
 	for (i = 0; i < idx - 1; i++)
+		if (!ptr)
+			return (NULL);
 		ptr = ptr->next;
 	if (idx != 0)
 	{
@@ -28,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	else
 	{
-		new_node->next = *head;
+		new_node->next = ptr;
 		*head = new_node;
 	}
 	return (new_node);

4fc98cbbf2dde65780d6a6efd05450e92e7d8cc5
sungnga <708@holbertonschool.com>
2019-03-27 07:22:24 +0000

    changed the order for n

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (!ptr)
 			return (NULL);
-		if (i == idx)
+		if (i == idx - 1)
 			break;
 		ptr = ptr->next;
 	}

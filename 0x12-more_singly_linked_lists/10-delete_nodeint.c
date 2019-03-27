261ab8d513e10f293dc1451b77fe175ed4f1abe4
sungnga <708@holbertonschool.com>
2019-03-27 07:58:05 +0000

    correct spelling error for index

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,7 +3,7 @@
 /**
  * delete_nodeint_at_index - deletes the node at a given position
  * @head: double pointer
- * @indx: index of the list where the new node should be added
+ * @index: index of the list where the new node should be added
  * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index)
 	{
 		prev = ptr->next;
-		free (ptr->next);
+		free(ptr->next);
 		ptr->next = prev;
 	}
 	else

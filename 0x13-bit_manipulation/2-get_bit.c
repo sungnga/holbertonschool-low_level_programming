610ec13a98558f1b24a62891cad6db66e5a098c8
sungnga <708@holbertonschool.com>
2019-03-29 03:40:54 +0000

    initial commit

---
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: a number
+ * @index: is the index
+ * Return: the value of the bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+
+	if (index > 63)
+		return (-1);
+	return (n >> index & 1);
+}

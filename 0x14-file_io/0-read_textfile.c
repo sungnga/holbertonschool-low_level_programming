9813c667815cad68dcdc71294db8a2d14ca54acf
sungnga <708@holbertonschool.com>
2019-04-02 21:42:17 +0000

    added function description

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -2,8 +2,8 @@
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename - pointer to text in a file
- * @letter - number of letters
+ * @filename: pointer to text in a file
+ * @letters: number of letters
  * Return: the actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)

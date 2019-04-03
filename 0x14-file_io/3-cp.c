073f140bad405a7c73b256bb497d3415a02ffa8e
sungnga <708@holbertonschool.com>
2019-04-03 04:01:55 +0000

    changed stardard error to macro

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -13,38 +13,32 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	f1 = open(argv[1], O_RDONLY);
 	if (f1 == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	while ((n = read(f1, buf, 1024)) > 0)
 	{
-		if (write(f2, buf, n) != n)
+		if (write(f2, buf, n) != n || f2 == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (f2 == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
 	if (close(f1) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", f1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
 		exit(100);
 	}
 	if (close(f2) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", f2);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
 		exit(100);
 	}
 	return (0);

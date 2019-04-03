7bcd4f88a1e0cbe1761951103636785624acb337
sungnga <708@holbertonschool.com>
2019-04-03 09:02:20 +0000

    initial commit

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @str: pointer to a string
+ * Return: a string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char *s1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *s2 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}

4d56236b1e0f02c0e8d0c839a1ccc0096adf6ae1
sungnga <708@holbertonschool.com>
2019-06-02 23:34:19 +0000

    Add comment

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - a djb2 function
+ * hash_djb2 - djb2 hash function that converts a string to hash value
  * @str: string to be converted to hash value
  *
  * Return: unsigned long hash value
@@ -13,6 +13,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
+	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
 	return (hash);
 }

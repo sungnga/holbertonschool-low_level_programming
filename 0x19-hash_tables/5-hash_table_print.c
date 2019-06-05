2c5f8b1fc12c7d0188242dfe1ad909d7fd0b9671
sungnga <708@holbertonschool.com>
2019-06-05 03:22:27 +0000

    Initial commit

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - A function that prints a hash table
+ * @ht: The hash table
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current = NULL;
+	unsigned long int i;
+	int flag = 1;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (flag == 1)
+			{
+				printf("'%s': '%s'", current->key, current->value);
+				flag = 0;
+			}
+			else
+				printf(", '%s': '%s'", current->key, current->value);
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}

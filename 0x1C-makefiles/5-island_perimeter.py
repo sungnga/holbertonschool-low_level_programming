04157335f47432f986f48fba1372a5f4e9a51a63
sungnga <708@holbertonschool.com>
2019-07-12 07:25:15 +0000

    Initial commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Returns the perimeter of the island described in grid"""
+
+def island_perimeter(grid):
+    """A method that returns the perimeter"""
+    size = len(row)
+    for i, row in enumerate(grid):
+        for j, val in enumerate(row):
+            if val == 1:
+                if grid[]

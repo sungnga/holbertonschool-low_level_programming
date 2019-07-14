7a329945159d18642d2aa795bfe454790c09fed6
sungnga <708@holbertonschool.com>
2019-07-14 05:52:07 +0000

    Include more conditions to sum up the perimeters

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Returns the perimeter of the island described in grid"""
+"""A module for the island_perimeter method"""
 
 
 def island_perimeter(grid):
     """A method that returns the perimeter"""
-    size = len(row)
+    count = 0
     for i, row in enumerate(grid):
         for j, val in enumerate(row):
             if val == 1:
-                if grid[]
+                if grid[i][j-1] != 1:
+                    count += 1
+                if grid[i][j+1] != 1:
+                    count += 1
+                if grid[i-1][j] != 1:
+                    count += 1
+                if grid[i+1][j] != 1:
+                    count += 1
+    return count

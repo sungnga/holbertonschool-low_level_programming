54038c5ffed9bc60220ccae334860cdfb0937217
sungnga <708@holbertonschool.com>
2019-07-24 20:50:30 +0000

    Utilize helper function binary_tree_node

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -16,24 +16,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
 	if (!parent->left)
 	{
 		parent->left = new_node;
-		new_node->n = value;
 		return (new_node);
-		free(new_node);
 	}
 
 	parent->left->parent = new_node;
 	new_node->parent = parent;
 	new_node->left = parent->left;
 	parent->left = new_node;
-
-	new_node->n = value;
 	return (new_node);
-	free(new_node);
 }

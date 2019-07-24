635a974c9ff1e06ad3ee03cecd2625768451af59
sungnga <708@holbertonschool.com>
2019-07-24 21:51:13 +0000

    Initial commit

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,5 +1,29 @@
 #include "binary_trees.h"
-size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_height - A function that measures the height of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: The value of height
+ */
+int tree_height(const binary_tree_t *tree, int *a, int *b)
+{
+	int left_height;
+	int right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = tree_height(tree->left, a, b);
+	right_height = tree_height(tree->right, a, b);
+	*a = left_height;
+	*b = right_height;
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
 
 /**
  * binary_tree_balance - A function that measures the balance factor
@@ -10,39 +34,11 @@ size_t binary_tree_height(const binary_tree_t *tree);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int a, b;
+	int a = 0, b = 0;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
-	a = binary_tree_height(tree->left);
-	b = binary_tree_height(tree->right);
+
+	tree_height(tree, &a, &b);
 	return (a - b);
-}
-
-
-/**
- * binary_tree_height - A function that measures the height of a binary tree
- * @tree: A pointer to the root node of the tree to measure the height.
- *
- * Return: The value of height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-        size_t left_height = 0;
-        size_t right_height = 0;
-
-        if (tree == NULL)
-                return (0);
-
-        if (tree->left == NULL && tree->right == NULL)
-                return (0);
-
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
-        if (left_height > right_height)
-                return (left_height += 1);
-        else
-                return (right_height += 1);
 }

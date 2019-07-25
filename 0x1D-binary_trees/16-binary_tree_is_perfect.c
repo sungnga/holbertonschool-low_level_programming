e52cdaf65d6b422fe0a63aea99c334fa246356b2
sungnga <708@holbertonschool.com>
2019-07-25 01:11:24 +0000

    Use find height helper function instead of find depth helper function

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * tree_height - A function that measures the height of a binary tree
+ * binary_tree_height - A function that measures the height of a binary tree
  * @tree: A pointer to the root node of the tree to measure the height.
  *
  * Return: The value of height
  */
-int tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height;
-	int right_height;
+	size_t left_height;
+	size_t right_height;
 
 	if (tree == NULL)
 		return (0);
@@ -17,8 +17,8 @@ int tree_height(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	left_height = tree_height(tree->left);
-	right_height = tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
@@ -61,8 +61,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = tree_height(tree->left);
-	right_height = tree_height(tree->right);
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
 
 	left_is_full = binary_tree_is_full(tree->left);
 	right_is_full = binary_tree_is_full(tree->right);
